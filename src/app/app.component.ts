997b20dbcb2cf4b94283b85b27cb5959e53ef4ce
FoxGeoff <fox.geoffrey@gmail.com>
2018-10-03 11:20:59 -0700

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HttpApi';
+
+  apiRoot: string = "http://httpbin.org";
+
+  constructor(private http: Http) { }
+
+  doGET() {
+    console.log("GET");
+  }
+
+  doPOST() {
+    console.log("POST");
+  }
+
+  doPUT() {
+    console.log("PUT");
+  }
+
+  doDELETE() {
+    console.log("DELETE");
+  }
+
+  doGETAsPromise() {
+    console.log("GET AS PROMISE");
+  }
+
+  doGETAsPromiseError() {
+    console.log("GET AS PROMISE ERROR");
+  }
+
+  doGETAsObservableError() {
+    console.log("GET AS OBSERVABLE ERROR");
+  }
+
+  doGETWithHeaders() {
+    console.log("GET WITH HEADERS");
+  }
 }
